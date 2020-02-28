30f7b748ddb031925ae32c4340c3faf70b7c0d58
Wesley Freire <wesley.freire.404@gmail.com>
2020-02-28 10:22:02 -0300

    3iguais

---
+++ 3-iguais.hs
@@ -0,0 +1,14 @@
+main = do
+    line1 <- getLine 
+    line2 <- getLine
+    line3 <- getLine
+    let x = read line1 :: Int
+    let y = read line2 :: Int
+    let	z = read line3 :: Int
+    print $ iguais3 x y z
+
+iguais3 x y z :: 
+                | x == y && x == z = "3 iguais"
+                | x /= y && x == z || x == y && x /= z || y == z && y /= x = "2 iguais"
+                 | otherwise = "nenhum"
+
